ca0bd06daf358f268952a8014d66ef697e5f853d
Simo Khouili <m.simokhouili@gmail.com>
2019-11-23 18:22:40 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,5 +1,4 @@
 def display_rainbow['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
   puts "R: red [0], O: orange [1], Y: yellow[2], G: green [3], B: blue [4] , I: indigo [5], V: violet[6]
-  
-end 
 
+end
